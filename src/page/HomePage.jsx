848f72ed420308478ffe151a7517c361fec489ab
RuslanGon <ruslan441006@gmail.com>
2024-12-12 22:09:10 +0200

    wer

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,9 +1,12 @@
 import css from "./HomePage.module.css"
+import { Link } from 'react-router-dom';
+
 
 const HomePage = () => {
   return (
     <div className={css.home}>
       <h1 className={css.title}>Ласкаво просимо <br /> у світ Гаррі Поттера</h1>
+      <Link className={css.link} to="/">Показати всіх персонажів</Link>
     </div>
   );
 };
