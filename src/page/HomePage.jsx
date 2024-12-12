353cd16f4647424e14eefa76771cd99dc51eb4ef
RuslanGon <ruslan441006@gmail.com>
2024-12-12 22:56:37 +0200

    wer

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,13 +1,14 @@
 import css from "./HomePage.module.css"
 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 
 const HomePage = () => {
   return (
     <div className={css.home}>
       <h1 className={css.title}>Ласкаво просимо <br /> у світ Гаррі Поттера</h1>
-      <Link className={css.link} to="/">Показати всіх персонажів</Link>
+      <button className={css.link}>Показати всіх персонажів</button>
+      {/* <Link className={css.link} to="/home">Показати всіх персонажів</Link> */}
     </div>
   );
 };
