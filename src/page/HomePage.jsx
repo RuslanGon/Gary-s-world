3bc08c00ea38de8060ee5c7e666be91723eb1bbe
RuslanGon <ruslan441006@gmail.com>
2024-12-13 11:50:53 +0200

    wer

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,25 +1,27 @@
 import { useState } from "react";
-import css from "./HomePage.module.css"
-import TitlePage from "./TitlePage.jsx";
-
-// import { Link } from 'react-router-dom';
-
+import css from "./HomePage.module.css";
+import TitlePage from "./TitlePage";
 
 const HomePage = () => {
-
-  const [pages, setPages] = useState([]);
+  const [showTitlePage, setShowTitlePage] = useState(false);
 
   const handleButtonClick = () => {
-    setPages((prevPages) => [...prevPages, <TitlePage key={prevPages.length} />]);
+    if (!showTitlePage) {
+      setShowTitlePage(true); 
+    }
   };
+
   return (
     <div className={css.home}>
       <h1 className={css.title}>Ласкаво просимо <br /> у світ Гаррі Поттера</h1>
-      <button className={css.link} onClick={handleButtonClick}>Показати всіх персонажів</button>
-      {/* <Link className={css.link} to="/home">Показати всіх персонажів</Link> */}
-      <div className={css.content}>
-        {pages}
-      </div>
+      <button className={css.link} onClick={handleButtonClick}>
+        Показати всіх персонажів
+      </button>
+      {showTitlePage && (
+        <div>
+          <TitlePage />
+        </div>
+      )}
     </div>
   );
 };
