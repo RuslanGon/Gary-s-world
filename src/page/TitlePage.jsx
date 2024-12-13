b91c2d7ac458dfe3a229bcc4a40556c6c749d877
RuslanGon <ruslan441006@gmail.com>
2024-12-13 11:40:10 +0200

    wer

--- src/page/TitlePage.jsx
+++ src/page/TitlePage.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import css from './TitlePage.module.css'
 import image1 from '../../images/image1.png'
 import image2 from '../../images/image2.png'
 import image3 from '../../images/image3.png'
@@ -6,18 +7,18 @@ import image3 from '../../images/image3.png'
 
 const TitlePage = () => {
   return (
-    <ul>
+    <ul className={css.list}>
       <li>
-        <img src={image1} alt="" />
-        <Link></Link>
+        <img className={css.img} src={image1} alt="" />
+        <Link className={css.link}>Студенти Гоґвортсу</Link>
       </li>
       <li>
-        <img src={image2} alt="" />
-        <Link></Link>
+        <img className={css.img}  src={image2} alt="" />
+        <Link className={css.link}>Співробітники Гоґвортсу</Link>
       </li>
       <li>
-        <img src={image3} alt="" />
-        <Link></Link>
+        <img className={css.img}  src={image3} alt="" />
+        <Link className={css.link}>Персонажі в певному будинку</Link>
       </li>
     </ul>
   );
